dc85efb7ac4889564fb2b7a93303e6b700a15c7d
AbhishekSanthoshJaya <59876352+AbhishekSanthoshJaya@users.noreply.github.com>
2020-03-24 19:07:27 -0400

    History
    Added button for history, moved data to next VC

--- WebViewExample/ViewController.swift
+++ WebViewExample/ViewController.swift
@@ -17,6 +17,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         loadLambtonUrl()
+        let history = myWebKitView.backForwardList
+        
     }
     
     
@@ -34,7 +36,13 @@ class ViewController: UIViewController {
         
     }
     
-    
+     @IBAction func btnHistory(_ sender: Any)
+    {
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        let historyTVC = sb.instantiateViewController(identifier: "historyTVC") as! HistoryTableViewController
+        historyTVC.historyList = self.history
+        navigationController?.pushViewController(historyTVC, animated: true)
+    }
 
     @IBAction func btnNavigation(_ sender: UIBarButtonItem)
     {
